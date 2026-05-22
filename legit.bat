ff3c1a1eaa2de72343f240aaf00c7d8cb43efcb5
TheGentlemannn <superikerh@hotmail.com>
2026-05-21 18:25:37 -0600

    update

--- legit.bat
+++ legit.bat
@@ -4,5 +4,7 @@ reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemProductName /t REG_SZ /
 reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v BIOSVendor /t REG_SZ /d "American Megatrends Inc." /f
 reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v BIOSVersion /t REG_SZ /d "F10" /f
 reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v BIOSReleaseDate /t REG_SZ /d "12/01/2021" /f
-echo Listo! Reinicia la VM.
-pause
+reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemBiosVersion /t REG_SZ /d "F10" /f
+reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v VideoBiosVersion /t REG_SZ /d "GOP 1.0" /f
+reg add "HKLM\HARDWARE\DESCRIPTION\System" /v SystemBiosVersion /t REG_SZ /d "ASUS - F10" /f
+reg add "HKLM\HARDWARE\DESCRIPTION\System" /v VideoBiosVersion /t REG_SZ /d "GOP 1.0" /f
--- verify.ps1
+++ verify.ps1
@@ -1,1 +1,3 @@
-Get-WmiObject -Class Win32_BIOS | Select-Object Manufacturer, SMBIOSBIOSVersion, ReleaseDate
+Get-Process | Where-Object {$_.Name -like "*vmware*" -or $_.Name -like "*vm*"} | Select-Object Name, Id
+
+Get-Service | Where-Object {$_.Name -like "*vmware*" -or $_.Name -like "*vm*"} | Select-Object Name, Status, DisplayName
