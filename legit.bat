2dd9753426090dbd310e19da42f85feef6130700
TheGentlemannn <superikerh@hotmail.com>
2026-05-21 18:33:00 -0600

    updated bat

--- legit.bat
+++ legit.bat
@@ -8,3 +8,5 @@ reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemBiosVersion /t REG_SZ /
 reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v VideoBiosVersion /t REG_SZ /d "GOP 1.0" /f
 reg add "HKLM\HARDWARE\DESCRIPTION\System" /v SystemBiosVersion /t REG_SZ /d "ASUS - F10" /f
 reg add "HKLM\HARDWARE\DESCRIPTION\System" /v VideoBiosVersion /t REG_SZ /d "GOP 1.0" /f
+reg add "HKLM\HARDWARE\DEVICEMAP\Scsi\Scsi Port 0\Scsi Bus 0\Target Id 0\Logical Unit Id 0" /v "Identifier" /t REG_SZ /d "Samsung SSD 870 EVO 500GB" /f
+reg add "HKLM\HARDWARE\DEVICEMAP\Scsi\Scsi Port 0\Scsi Bus 0\Target Id 0\Logical Unit Id 0" /v "SerialNumber" /t REG_SZ /d "S59ANX0R816533M" /f
