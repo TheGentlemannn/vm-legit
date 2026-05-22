b7ff11b6957df49e623b25da4a0c5331f50464e9
TheGentlemannn <superikerh@hotmail.com>
2026-05-21 19:08:26 -0600

    updated bat

--- verify.ps1
+++ verify.ps1
@@ -5,3 +5,9 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard" /v EnableVirtualizat
 
 # Paso 3 - Deshabilitar driver blocklist
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\CI\Config" /v VulnerableDriverBlocklistEnable /t REG_DWORD /d 0 /f
+
+
+Write-Host "VBS Status:" (Get-CimInstance -ClassName Win32_DeviceGuard -Namespace 'root\Microsoft\Windows\DeviceGuard').VirtualizationBasedSecurityStatus
+Write-Host "HVCI Enabled:" (Get-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity' -ErrorAction SilentlyContinue).Enabled
+Write-Host "VBS Enabled:" (Get-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\DeviceGuard' -ErrorAction SilentlyContinue).EnableVirtualizationBasedSecurity
+Write-Host "Blocklist:" (Get-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\CI\Config' -ErrorAction SilentlyContinue).VulnerableDriverBlocklistEnable
